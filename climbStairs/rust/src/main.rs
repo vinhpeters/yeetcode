a97ba7205c89fba02256486b555a9470c8deb25c
Sharif.T <Shariftarver@gmail.com>
2023-03-17 03:57:08 -0400

    climbstairs implementation with tdd

--- climbStairs/rust/src/main.rs
+++ climbStairs/rust/src/main.rs
@@ -1,3 +1,41 @@
 fn main() {
-    println!("Hello, world!");
+    climb_stairs(2);
 }
+
+
+fn climb_stairs(n: i32) -> i32 {
+    if n <= 2 {
+        return n;
+    }
+
+    let mut dp = vec![0; n as usize + 1];
+    dp[1] = 1;
+    dp[2] = 2;
+
+    for i in 3..=n as usize {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+
+    dp[n as usize]
+}
+
+#[cfg(test)]
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_climb_stairs() {
+        assert_eq!(climb_stairs(2), 2);
+        assert_eq!(climb_stairs(3), 3);
+        assert_eq!(climb_stairs(4), 5);
+        assert_eq!(climb_stairs(5), 8);
+        assert_eq!(climb_stairs(6), 13);
+        assert_eq!(climb_stairs(7), 21);
+    }
+}
+
+
+
+
+
